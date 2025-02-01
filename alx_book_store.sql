09c0bacde9d04f282fcdef07e54971606f43351b
EL-YAMANI Nassr <elyamani.n@daikineurope.com>
2025-02-01 13:00:08 +0100

    m

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,20 +3,20 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 
 CREATE TABLE Books(
-    book_id int PRIMARY KEY,
+    book_id INT PRIMARY KEY,
     title VARCHAR(130),
-    author_id int FOREIGN KEY REFERENCES Aothors(aothor_id),
-    price double,
-    publication_date date,
+    author_id INT FOREIGN KEY REFERENCES Aothors(aothor_id),
+    price DOUBLE,
+    publication_date DATE,
 );
 
 CREATE TABLE Authors(
-    author_id int PRIMARY KEY,
+    author_id INT PRIMARY KEY,
     author_name VARCHAR(215),
 );
 
 CREATE TABLE Customers(
-    customer_id int PRIMARY KEY,
+    customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT,
@@ -30,9 +30,11 @@ CREATE TABLE Orders(
 );
 
 CREATE TABLE Order_Details(
-    order_deatail_id int PRIMARY KEY,
-    order_id int FOREIGN KEY REFERENCES Orders(order_id),
-    book_id int FOREIGN KEY REFERENCES Books(book_id),
-    quantity double,
+    order_deatail_id INT PRIMARY KEY,
+    order_id INT FOREIGN KEY REFERENCES Orders(order_id),
+    book_id INT FOREIGN KEY REFERENCES Books(book_id),
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
