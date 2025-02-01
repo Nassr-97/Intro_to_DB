f47fbb70c482f2fdd6ea6d84db1b61c143302f04
EL-YAMANI Nassr <elyamani.n@daikineurope.com>
2025-02-01 12:58:12 +0100

    m

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -23,9 +23,9 @@ CREATE TABLE Customers(
 );
 
 CREATE TABLE Orders(
-    order_id int PRIMARY KEY,
-    customer_id int,
-    order_date date,
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
