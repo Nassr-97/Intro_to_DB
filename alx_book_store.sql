cb1e5c0156071da34b398a350bbbad6f0027b62d
EL-YAMANI Nassr <elyamani.n@daikineurope.com>
2025-02-01 12:52:09 +0100

    m

---
+++ alx_book_store.sql
@@ -0,0 +1,37 @@
+
+CREATE DATABASE alx_book_store;
+
+
+CREATE TABLE Books(
+    book_id int PRIMARY KEY,
+    title VARCHAR(130),
+    author_id int FOREIGN KEY REFERENCES Aothors(aothor_id),
+    price double,
+    publication_date date,
+);
+
+CREATE TABLE Authors(
+    author_id int PRIMARY KEY,
+    author_name VARCHAR(215),
+);
+
+CREATE TABLE Customers(
+    customer_id int PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address text,
+);
+
+CREATE TABLE Orders(
+    order_id int PRIMARY KEY,
+    customer_id int FOREIGN KEY REFERENCES Customers(customer_id),
+    order_date date,
+);
+
+CREATE TABLE Order_Details(
+    order_deatail_id int PRIMARY KEY,
+    order_id int FOREIGN KEY REFERENCES Orders(order_id),
+    book_id int FOREIGN KEY REFERENCES Books(book_id),
+    quantity double,
+);
+
